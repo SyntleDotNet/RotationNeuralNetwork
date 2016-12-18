626978f567cf1e67075b2ea6b6dc0cab33edf751
Micah <Micah@DESKTOP-G4BE983.lan>
2016-12-18 19:18:05 +0000

    Added basic functions.

--- Syntle/RotationNeuralNetwork/src/neuralnet/AI.java
+++ Syntle/RotationNeuralNetwork/src/neuralnet/AI.java
@@ -1,5 +1,38 @@
 package neuralnet;
 
-public class AI {
+import java.util.ArrayList;
 
+import game.Player;
+
+public class AI
+{
+	ArrayList<Double> input = new ArrayList<Double>();
+	Player player;
+
+	public void Init(Player player)
+	{
+		this.player = player;
+		input.add(player.getX());
+		input.add((double) 0);
+		input.add((double) 0);
+	}
+
+	public void UpdateLowestGap(double x, double y)
+	{
+		input.set(1, x);
+		input.set(2, y);
+	}
+
+	public void Update(double objectiveValue)
+	{
+		input.set(0, player.getX());
+
+		FeedForward(input);
+	}
+
+	void FeedForward(ArrayList<Double> inputData)
+	{
+		player.moveRight();
+		player.moveLeft();
+	}
 }
